54d12f7dc3815fd3b3b5440259bc74c056c81abe
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-06-25 10:19:06 +0200

    [FSK-404] component isn't finished but needed for routing tests

--- src/components/PageLayout/Home.tsx
+++ src/components/PageLayout/Home.tsx
@@ -1,14 +1,28 @@
+import { Page } from "fsxa-api";
+
 interface HomeProps {
-  label: string;
   primary?: boolean;
+  path: string;
 }
 
 export const Home = ({
-  label,
   primary = true,
+  path,
 }: HomeProps) => {
   const mode = primary ? 'storybook-home--primary' : 'storybook-home--secondary';
+
+  //TODO define variable page of type Page
+  //const top = () => {
+  //  return page.children.find((pagebody) => pagebody.name === 'top')!
+  //}
+  //const content = () => {
+  //  return page.children.find((pagebody) => pagebody.name === 'content')!
+  //}
+  
+  //TODO (below) two PageBody tags
   return(
-    <h1>{ label }</h1>
+    <div data-testid="homePageLayout">
+      <h1>{path}</h1>
+    </div>
   )
 }
