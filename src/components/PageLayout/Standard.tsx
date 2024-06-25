a01e2f2a33535f5910c9eaf1aabe7f45b369deb9
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-06-25 10:21:52 +0200

    [FSK-405] component not finished but needed for routing tests

--- src/components/PageLayout/Standard.tsx
+++ src/components/PageLayout/Standard.tsx
@@ -1,5 +1,22 @@
-function Standard() {
-  return()
+import { Page } from 'fsxa-api';
+
+interface StandardProps {
+  primary?: boolean;
+  path: string;
 }
 
-export default Standard;
+export const Standard = ({
+  primary = true,
+  path,
+}: StandardProps) => {
+  const mode = primary ? 'storybook-standard--primary' : 'storybook-standard--secondary';
+  //TODO define page of type Page
+``//TODO (below) add SectionHeader and PageBody
+  return(
+    <>
+    <div data-testid="standardPageLayout">
+      <h1>{ path }</h1>
+    </div>
+    </>
+  )
+}
