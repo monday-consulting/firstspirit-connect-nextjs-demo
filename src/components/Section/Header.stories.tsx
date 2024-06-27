b81dabf14b6f5f3a0cf6030fc9f9d4deba98064d
John Rodriguez <john.rodriguez@monday-consulting.com>
2024-06-27 15:03:11 +0200

    FSK-412: add type to story

--- src/components/Section/Header.stories.tsx
+++ src/components/Section/Header.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
 import Header from "./Header";
 
-const meta = {
+const meta: Meta<typeof Header> = {
     title: "components/Section/Header",
     component: Header,
     parameters: {
