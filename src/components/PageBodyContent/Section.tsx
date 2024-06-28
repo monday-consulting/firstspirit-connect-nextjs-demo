cb0f29487327ab9f1ec49394f06316955da23f0d
John Rodriguez <john.rodriguez@monday-consulting.com>
2024-06-28 11:27:57 +0200

    FSK-403: add section component and add optional data to unknown

--- src/components/PageBodyContent/Section.tsx
+++ src/components/PageBodyContent/Section.tsx
@@ -1,5 +1,66 @@
-function Section() {
-  return()
+import React, { useMemo } from "react";
+import { Section } from "fsxa-api";
+import Unknown from "../Unknown";
+import { useNextApp } from "../tests/testutils/nextMocks";
+
+interface SectionProps {
+    content: Section;
 }
+
+const Section = ({ content }: SectionProps) => {
+    const { $isPreviewMode } = useNextApp();
+
+    const sectionComponent = useMemo(() => {
+        switch (content.sectionType) {
+            case "smartliving.product_overview":
+                return "SectionProductOverview";
+            case "text_image":
+                return "SectionTextImage";
+            case "product_category_teaser":
+                return "SectionProductCategoryTeaser";
+            case "steps":
+                return "SectionSteps";
+            case "accordion":
+                return "SectionAccordion";
+            case "stage":
+                return "SectionStage";
+            case "features":
+                return "SectionFeatures";
+            case "interesting_facts":
+                return "SectionInterestingFacts";
+            case "products.category_products":
+                return "SectionProductCategory";
+            case "smartliving.product":
+                return "SectionProduct";
+            case "teaser":
+                return "SectionTeaser";
+            case "featured_products":
+                return "SectionFeaturedProducts";
+            default:
+                return undefined;
+        }
+    }, [content.sectionType]);
+
+    return (
+        <div data-testid="section">
+            {sectionComponent ? (
+                <div
+                    data-preview-id={
+                        content.previewId?.split(".")[0] !== ""
+                            ? content.previewId
+                            : undefined
+                    }
+                >
+                    {sectionComponent}
+                </div>
+            ) : (
+                $isPreviewMode &&
+                !sectionComponent && (
+                    <Unknown content={content} data={content.data} />
+                )
+            )}
+        </div>
+    );
+};
 
 export default Section;
