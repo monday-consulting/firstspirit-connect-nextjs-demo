a2cd79bb8e2cc9df2295e632b6f20027ff5d87b1
Alec Winter <alec.winter@monday-consulting.com>
2024-07-10 17:02:29 +0200

    feat: add convetional commits + husky git hook

--- src/components/utils/misc.ts
+++ src/components/utils/misc.ts
@@ -4,29 +4,29 @@ export function getLanguageNamesFromLocales(identifiers: string[]): LocaleType[]
   const locales: LocaleType[] = [];
 
   for (const identifier of identifiers) {
-      // split the identifier into language and region
-      // de_DE => [de, DE]
-      const [isoCode] = identifier.split("_");
-  
-      if (isoCode) {
-          try {
-              const intl = new Intl.DisplayNames([isoCode], {
-                  type: "language",
-              });
-              const languageName = intl.of(isoCode) ?? null;
-  
-              locales.push({
-                  name: languageName,
-                  identifier,
-              });
-          } catch (error) {
-              console.error(`Error creating Intl.DisplayNames for ${isoCode}:`, error);
-              locales.push({
-                  name: null,
-                  identifier,
-              });
-          }
+    // split the identifier into language and region
+    // de_DE => [de, DE]
+    const [isoCode] = identifier.split("_");
+
+    if (isoCode) {
+      try {
+        const intl = new Intl.DisplayNames([isoCode], {
+          type: "language",
+        });
+        const languageName = intl.of(isoCode) ?? null;
+
+        locales.push({
+          name: languageName,
+          identifier,
+        });
+      } catch (error) {
+        console.error(`Error creating Intl.DisplayNames for ${isoCode}:`, error);
+        locales.push({
+          name: null,
+          identifier,
+        });
       }
+    }
   }
 
   return locales;
