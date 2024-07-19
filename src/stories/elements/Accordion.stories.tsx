954acd1b4b41a4459cee2e9d61152288b9a4f24d
John Rodriguez <john.rodriguez@monday-consulting.com>
2024-07-19 13:23:39 +0200

    feat(NLFY-47): commit

--- src/stories/elements/Accordion.stories.tsx
+++ src/stories/elements/Accordion.stories.tsx
@@ -2,19 +2,19 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Accordion } from "@/components/elements/Accordion";
 
 const meta = {
-    title: "components/Elements/Accordion",
-    component: Accordion,
-    tags: ["autodocs"],
+  title: "components/Elements/Accordion",
+  component: Accordion,
+  tags: ["autodocs"],
 } satisfies Meta<typeof Accordion>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-    args: {
-        primary: true,
-        data: {
-            st_title: "Accordion",
-        },
+  args: {
+    primary: true,
+    data: {
+      st_title: "Accordion",
     },
+  },
 };
