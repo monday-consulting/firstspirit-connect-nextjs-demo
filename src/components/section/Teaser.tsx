b9845a9ae6a62e8329c98c52e55370316284d13b
Alec Winter <alec.winter@monday-consulting.com>
2024-07-24 11:02:40 +0200

    fix(NLFY-65): remove data object from teaser

--- src/components/section/Teaser.tsx
+++ src/components/section/Teaser.tsx
@@ -1,98 +1,100 @@
 import Link from "next/link";
 import Image from "next/image";
+import type { ReactNode } from "react";
 
-export interface Teaser {
-  st_headline: string;
-  st_image?: {
+export interface TeaserProps {
+  headline: string;
+  image?: {
     src: string;
     alt: string;
   };
-  st_text: string;
-  st_cta?: {
-    ref: string;
+  text: ReactNode | string;
+  cta?: {
+    href: string;
     linkText: string;
   };
-}
-
-export interface TeaserProps {
-  data: Teaser;
   imageLeft: boolean;
 }
 
-const Teaser = ({ data, imageLeft }: TeaserProps) => {
+const Teaser = ({ headline, image, text, cta, imageLeft }: TeaserProps) => {
   return (
-    <>
-      <section className="bg-white py-14">
-        <div className="container mx-auto px-4">
-          {!imageLeft && (
-            <>
-              <div className="-mx-4 flex flex-wrap items-center">
-                <div className="mt-8 px-4 text-left sm:w-full md:w-1/2">
-                  {data.st_headline && (
-                    <h2 className="mb-8 font-bold font-heading text-3xl text-primary leading-none tracking-px-n md:text-4xl">
-                      {data.st_headline}
-                    </h2>
-                  )}
-                  {data.st_text && <p className="mb-5 text-text">{data.st_text}</p>}
-                  <div className="flex flex-wrap">
-                    {data.st_cta && (
-                      <Link href={data.st_cta.ref}>
-                        <div className="w-full py-1 md:mr-4 md:w-auto md:py-0">
-                          <span className="inline-block w-full rounded-md bg-secondary px-7 py-5 text-center font-medium text-base text-white leading-4 hover:brightness-90 md:text-lg">
-                            {data.st_cta.linkText}
-                          </span>
-                        </div>
-                      </Link>
-                    )}
-                  </div>
-                </div>
-                <div className="px-4 sm:w-full md:w-1/2">
-                  <Image
-                    src={data.st_image?.src}
-                    alt={data.st_image?.alt}
-                    width={400}
-                    height={200}
-                    className="relative overflow-hidden rounded-xl"
-                  />
-                </div>
-              </div>
-            </>
-          )}
-          {imageLeft && (
-            <>
-              <div className="-mx-4 flex flex-wrap items-center">
-                <div className="px-4 sm:w-full md:w-1/2">
-                  <Image
-                    src={data.st_image?.src}
-                    alt={data.st_image?.alt}
-                    width={400}
-                    height={200}
-                    className="relative overflow-hidden rounded-xl"
-                  />
-                </div>
-                <div className="mt-8 px-4 text-left sm:w-full md:w-1/2">
-                  {data.st_headline && (
-                    <h2 className="mb-8 font-bold font-heading text-3xl text-primary leading-none tracking-px-n md:text-4xl">
-                      {data.st_headline}
-                    </h2>
-                  )}
-                  {data.st_text && <p className="mb-5 text-text">{data.st_text}</p>}
-                  <div className="flex flex-wrap">
-                    <Link href={data.st_cta?.ref}>
+    <section className="bg-white py-14">
+      <div className="container mx-auto px-4">
+        {!imageLeft && (
+          <>
+            <div className="-mx-4 flex flex-wrap items-center">
+              <div className="mt-8 px-4 text-left sm:w-full md:w-1/2">
+                {headline && (
+                  <h2 className="mb-8 font-bold font-heading text-3xl text-primary leading-none tracking-px-n md:text-4xl">
+                    {headline}
+                  </h2>
+                )}
+                {text && <div className="mb-5 text-text">{text}</div>}
+                <div className="flex flex-wrap">
+                  {cta && (
+                    <Link href={cta.href}>
                       <div className="w-full py-1 md:mr-4 md:w-auto md:py-0">
                         <span className="inline-block w-full rounded-md bg-secondary px-7 py-5 text-center font-medium text-base text-white leading-4 hover:brightness-90 md:text-lg">
-                          {data.st_cta?.linkText}
+                          {cta.linkText}
+                        </span>
+                      </div>
+                    </Link>
+                  )}
+                </div>
+              </div>
+              {image && (
+                <div className="px-4 sm:w-full md:w-1/2">
+                  <Image
+                    src={image.src}
+                    alt={image.alt}
+                    width={400}
+                    height={200}
+                    className="relative overflow-hidden rounded-xl"
+                  />
+                </div>
+              )}
+            </div>
+          </>
+        )}
+        {imageLeft && (
+          <>
+            <div className="-mx-4 flex flex-wrap items-center">
+              {image && (
+                <div className="px-4 sm:w-full md:w-1/2">
+                  <Image
+                    src={image.src}
+                    alt={image.alt}
+                    width={400}
+                    height={200}
+                    className="relative overflow-hidden rounded-xl"
+                  />
+                </div>
+              )}
+
+              <div className="mt-8 px-4 text-left sm:w-full md:w-1/2">
+                {headline && (
+                  <h2 className="mb-8 font-bold font-heading text-3xl text-primary leading-none tracking-px-n md:text-4xl">
+                    {headline}
+                  </h2>
+                )}
+                {text && <div className="mb-5 text-text">{text}</div>}
+                {cta && (
+                  <div className="flex flex-wrap">
+                    <Link href={cta.href}>
+                      <div className="w-full py-1 md:mr-4 md:w-auto md:py-0">
+                        <span className="inline-block w-full rounded-md bg-secondary px-7 py-5 text-center font-medium text-base text-white leading-4 hover:brightness-90 md:text-lg">
+                          {cta.linkText}
                         </span>
                       </div>
                     </Link>
                   </div>
-                </div>
+                )}
               </div>
-            </>
-          )}
-        </div>
-      </section>
-    </>
+            </div>
+          </>
+        )}
+      </div>
+    </section>
   );
 };
 export { Teaser };
--- src/stories/section/Teaser.stories.tsx
+++ src/stories/section/Teaser.stories.tsx
@@ -13,17 +13,15 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     imageLeft: true,
-    data: {
-      st_headline: "Headline",
-      st_image: {
-        src: "https://placehold.co/600x400",
-        alt: "Alt Text",
-      },
-      st_text: "Text",
-      st_cta: {
-        ref: "Home",
-        linkText: "Home",
-      },
+    headline: "Headline",
+    image: {
+      src: "https://placehold.co/600x400",
+      alt: "Alt Text",
+    },
+    text: "Textd wdawd wad awdjaklwj dlkawjdkl jawkldjaw lkdjalwökjdklaöwjdkajwlk ödjwakl djlkawjd lkwajdkl wajkdjwa kldjöklaw jdklwjd klwajdkl waj",
+    cta: {
+      href: "Home",
+      linkText: "Home",
     },
   },
 };
