5fe5c909235faff2e17bc906a367bfc4b7b12b4b
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-07-24 10:04:48 +0200

    feat(NLFY-16): added footer component and story

---
+++ src/stories/app-layout/Footer.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Footer } from "@/components/app-layout/Footer";
+
+const meta: Meta<typeof Footer> = {
+  title: "components/AppLayout/Footer",
+  component: Footer,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    copyrightText: "CopyrightText",
+    legalLinks: [
+      {
+        name: "Home",
+        route: "Home",
+      },
+      {
+        name: "Landing",
+        route: "Landing",
+      },
+    ],
+  },
+};
