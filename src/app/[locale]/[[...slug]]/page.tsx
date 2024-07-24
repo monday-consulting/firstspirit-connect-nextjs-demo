9d8fe8ca6fe8aca3177b50540829df2e6b338d47
Alec Winter <alec.winter@monday-consulting.com>
2024-07-24 15:57:08 +0200

    feat(Content): add basic content parsing to body

--- src/app/[locale]/[[...slug]]/page.tsx
+++ src/app/[locale]/[[...slug]]/page.tsx
@@ -1,83 +1,24 @@
+import { Home } from "@/components/page-layout/Home";
+import { StandardLayout } from "@/components/page-layout/StandardLayout";
 import { getPageContentById } from "@/gql/pageContent";
-
-type Option = {
-  type: "Option";
-  key: string;
-  value: string;
-};
-
-type LinkData = {
-  lt_link: {
-    type: "Reference";
-    referenceId: string;
-    referenceType: "PageRef";
-  };
-  lt_text: string;
-};
-
-type Link = {
-  type: "Link";
-  template: "internal_link";
-  data: LinkData;
-  meta: Record<string, unknown>;
-};
-
-type TextContent = {
-  type: "text";
-  content: string;
-  data: Record<string, unknown>;
-};
-
-type Paragraph = {
-  data: Record<string, unknown>;
-  content: TextContent[];
-  type: "paragraph";
-};
-
-type TextAlignment = {
-  type: "Option";
-  key: "left" | "right";
-  value: "Left" | "Right";
-  fsType: "Option";
-  label: "Left" | "Right";
-  identifier: "left" | "right";
-};
-
-type SectionData = {
-  st_category: Option;
-  st_category_link: Link;
-  st_headline: string;
-  st_sectionLifespanFrom: string | null;
-  st_sectionLifespanTo: string | null;
-  st_text: Paragraph[];
-  st_text_alignment: TextAlignment;
-};
-
-type Section = {
-  id: string;
-  type: "Section";
-  name: string;
-  displayName: string;
-  sectionType: string;
-  previewId: string;
-  data: SectionData;
-  displayed: boolean;
-  children: Section[];
-};
+import type { PageBody } from "fsxa-api";
 
 const SlugPage = async () => {
   const pageContent = await getPageContentById("10f3e2ca-b451-5ab0-b83b-eba3034c55ea");
-  const pageBodies = pageContent?.pageBodies?.map((item) => item?.children);
+  const pageBodies = pageContent?.pageBodies?.map((item) => item) as PageBody[];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* {pageBodies && (
-        <div>
-          {pageBodies?.map((body) => (
-            <p key={body.name}>{body.name}</p>
-          ))}
-        </div>
-      )} */}
+      {pageContent?.layout === "home" && (
+        <>
+          <Home path="test" />
+        </>
+      )}
+      {pageContent?.layout === "standard" && (
+        <>
+          <StandardLayout pageBodies={pageBodies} />
+        </>
+      )}
     </main>
   );
 };
--- src/components/section/ProductCategoryTeaser.tsx
+++ src/components/section/ProductCategoryTeaser.tsx
@@ -1,6 +1,25 @@
-// TODO: crate component
-function ProductCategoryTeaser() {
-  return null;
-}
+import {
+  CategoryProductsDescription,
+  type CategoryProductsDescriptionProps,
+} from "./CategoryProductsDescription";
 
-export default ProductCategoryTeaser;
+export type ProductCategoryTeaserProps = {
+  categoryProduct: CategoryProductsDescriptionProps;
+};
+const ProductCategoryTeaser = ({ categoryProduct }: ProductCategoryTeaserProps) => {
+  return (
+    <section>
+      <div className="radius-for-skewed bg-gray-100 py-20">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap items-center">
+            <CategoryProductsDescription {...categoryProduct} />
+            {/* TODO: Add component */}
+            {/* <SectionCategoryProductsList :category-id="data.st_category.key" /> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export { ProductCategoryTeaser };
