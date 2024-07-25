56a129b3bcca2a7235a73f6ecb27fc9bac8b37ec
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-07-25 09:51:10 +0200

    feat(NLFY-52): added features component and story

--- src/components/section/Features.tsx
+++ src/components/section/Features.tsx
@@ -1,6 +1,27 @@
-// TODO: crate component
-function Features() {
-  return null;
+import { Feature } from "@/components/elements/Feature";
+import type { FeaturesContent } from "@/components/elements/Feature";
+
+interface FeaturesProps {
+  headline: string;
+  text: string;
+  features: FeaturesContent[];
 }
 
-export default Features;
+const Features = ({ headline, text, features }: FeaturesProps) => {
+  return (
+    <section className="py-14">
+      <div className="container mx-auto px-4 text-center">
+        <h2 className="mb-8 font-bold font-heading text-3xl text-primary leading-none tracking-px-n md:text-4xl">
+          {headline}
+        </h2>
+        <p className="mb-6 font-semibold text-coolGray-500 text-xl leading-7">{text}</p>
+        <div className="-m-8 flex flex-wrap text-left">
+          {features.map((feature) => (
+            <Feature data={feature} key={feature.id} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export { Features };
---
+++ src/stories/section/Features.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Features from "@/components/section/Features";
+
+const meta: Meta<typeof Features> = {
+  title: "components/Section/Features",
+  component: Features,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    headline: "Headline",
+    text: "Text",
+    features: [
+      {
+        link: {
+          href: "Home",
+          linkText: "Home",
+        },
+        image: {
+          src: "https://placehold.co/600x400",
+          alt: "Alt Text",
+        },
+        title: "Title",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui turpis, maximus non commodo ut, fringilla nec libero.",
+        id: "ID1",
+      },
+      {
+        link: {
+          href: "Home",
+          linkText: "Home",
+        },
+        image: {
+          src: "https://placehold.co/600x400",
+          alt: "Alt Text",
+        },
+        title: "Title",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui turpis, maximus non commodo ut, fringilla nec libero.",
+        id: "ID2",
+      },
+    ],
+  },
+};
