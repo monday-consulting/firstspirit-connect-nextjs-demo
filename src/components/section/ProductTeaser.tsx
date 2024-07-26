82f8ca158aeda417bcb130368e53b7767c4ba20c
John Rodriguez <john.rodriguez@monday-consulting.com>
2024-07-26 10:05:53 +0200

    feat(NLFY-33): add getProductLink func

--- src/components/section/ProductTeaser.tsx
+++ src/components/section/ProductTeaser.tsx
@@ -5,31 +5,43 @@ import type { ProductOverviewItemProps } from "./ProductOverviewItem";
 import type { RichTextElementProps } from "../elements/RichTextElement";
 
 export interface ProductTeaserProps {
-  category: {
-    type: string;
-    key: string;
-    value: string;
-    products: ProductOverviewItemProps[];
+  categoryProduct: {
+    category: {
+      type: string;
+      key: string;
+      value: string;
+      products: ProductOverviewItemProps[];
+    };
+    category_link: {
+      href: string;
+      linkText: string;
+    };
+    headline: string;
+    text: RichTextElementProps[];
   };
-  category_link: {
-    href: string;
-    linkText: string;
-  };
-  headline: string;
-  text: RichTextElementProps[];
 }
 
-const ProductTeaser = ({ category, category_link, headline, text }: ProductTeaserProps) => {
-  const textContent = text.map((item) => item.content).join(" ");
+const getProductLink = (categoryProduct: ProductTeaserProps["categoryProduct"]) => {
+  return categoryProduct.category_link.href;
+};
 
+const ProductTeaser = ({ categoryProduct }: ProductTeaserProps) => {
+  const textContent = categoryProduct.text.map((item) => item.content).join(" ");
+  const productLink = getProductLink(categoryProduct);
   return (
     <div className="radius-for-skewed bg-gray-100 py-20">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-2 gap-20">
           <div className="m-auto">
-            <Teaser headline={headline} text={textContent} imageLeft={false} cta={category_link} />
+            <Teaser
+              headline={categoryProduct.headline}
+              text={textContent}
+              imageLeft={false}
+              cta={{ href: productLink, linkText: categoryProduct.category_link.linkText }}
+            />
           </div>
-          <ProductOverview category={category} />
+
+          <ProductOverview category={categoryProduct.category} />
         </div>
       </div>
     </div>
--- src/stories/section/ProductTeaser.stories.tsx
+++ src/stories/section/ProductTeaser.stories.tsx
@@ -15,59 +15,61 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    category: {
-      key: "Doorbell",
-      type: "idk",
-      value: "test",
-      products: [
-        {
-          headline: "Doorbell Example",
-          description:
-            "lorem  eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctusipsum...",
-          image: {
-            src: "https://placehold.co/600x400",
-            alt: "Alt Text",
+    categoryProduct: {
+      category: {
+        key: "Doorbell",
+        type: "idk",
+        value: "test",
+        products: [
+          {
+            headline: "Doorbell Example1",
+            description:
+              "lorem  eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctusipsum...",
+            image: {
+              src: "https://placehold.co/600x400",
+              alt: "Alt Text",
+            },
           },
-        },
-        {
-          headline: "Another Example",
-          description: "Another lorem...",
-          image: {
-            src: "https://placehold.co/600x400",
-            alt: "Another Alt Text",
+          {
+            headline: "Another Example1",
+            description: "Another lorem...",
+            image: {
+              src: "https://placehold.co/600x400",
+              alt: "Another Alt Text",
+            },
           },
-        },
-        {
-          headline: "Doorbell Example",
-          description: "lorem ipsum...",
-          image: {
-            src: "https://placehold.co/600x400",
-            alt: "Alt Text",
+          {
+            headline: "Doorbell Example2",
+            description: "lorem ipsum...",
+            image: {
+              src: "https://placehold.co/600x400",
+              alt: "Alt Text",
+            },
           },
-        },
-        {
-          headline: "Another Example",
-          description:
-            "ed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut lab...",
-          image: {
-            src: "https://placehold.co/600x400",
-            alt: "Another Alt Text",
+          {
+            headline: "Another Example2",
+            description:
+              "ed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut lab...",
+            image: {
+              src: "https://placehold.co/600x400",
+              alt: "Another Alt Text",
+            },
           },
+        ],
+      },
+      category_link: {
+        href: "exampleUrl.com",
+        linkText: "Explore all door locks",
+      },
+      text: [
+        {
+          content:
+            "ed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ",
+          data: "example",
+          type: "paragraph",
         },
       ],
+      headline: "Smart Door Locks",
     },
-    category_link: {
-      href: "exampleUrl.com",
-      linkText: "Explore all door locks",
-    },
-    text: [
-      {
-        content:
-          "ed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ",
-        data: "example",
-        type: "paragraph",
-      },
-    ],
-    headline: "Smart Door Locks",
   },
 };
