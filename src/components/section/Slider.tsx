eec8a2e3504347e70465f5fe4d4f146d76911f5d
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-07-29 15:12:32 +0200

    feat(NLFY-61): added Slider component and story

--- src/components/section/Slider.tsx
+++ src/components/section/Slider.tsx
@@ -1,6 +1,105 @@
-// TODO: crate component
-function Slider() {
-  return null;
+import { DevComponent } from "../Dev";
+import { Button, type ButtonT } from "../elements/Button";
+import Image from "next/image";
+import { useNextApp } from "../tests/testutils/nextMocks";
+import { useDev } from "../composables/showDev";
+import { useState } from "react";
+
+export interface SliderSlide {
+  button: ButtonT;
+  description: string;
+  picture: {
+    src: string;
+    alt: string;
+  };
+  title: string;
 }
 
-export default Slider;
+export interface SliderProps {
+  data: SliderSlide[];
+}
+
+const Slider = ({ data }: SliderProps) => {
+  const showDev = useDev();
+  const $isPreviousMode = useNextApp();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const activeSlide = data[activeImageIndex];
+
+  function nextSlide() {
+    setActiveImageIndex((activeImageIndex + 1) % data.length);
+  }
+  function prevSlide() {
+    setActiveImageIndex((activeImageIndex - 1 + data.length) % data.length);
+  }
+
+  return (
+    <div>
+      <div className="group relative">
+        {showDev && $isPreviousMode && (
+          <div className="hidden group-hover:block">
+            <DevComponent content={data} />
+          </div>
+        )}
+        {activeSlide && (
+          <div className="relative text-white">
+            <div className="sliderButtonContainer left-0">
+              <button className="sliderButton pr-8" onClick={prevSlide}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <title>previous</title>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 19.5L8.25 12l7.5-7.5"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="sliderButtonContainer right-0">
+              <button className="sliderButton pl-8" onClick={nextSlide}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <title>next</title>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-transparent p-6 md:p-16">
+              <div className="flex max-w-xl flex-col space-y-4 md:py-10">
+                <h1 className="font-black text-4xl md:text-6xl">{activeSlide.title}</h1>
+                <p>{activeSlide.description}</p>
+                <div>
+                  <Button data={activeSlide.button} />
+                </div>
+              </div>
+            </div>
+            <Image
+              className="h-96 w-full object-cover md:h-[600px]"
+              src={activeSlide.picture.src}
+              alt={activeSlide.picture.alt}
+              width={400}
+              height={400}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export { Slider };
---
+++ src/stories/section/Slider.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Slider } from "@/components/section/Slider";
+
+const meta = {
+  title: "components/Section/Slider",
+  component: Slider,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Slider>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    data: [
+      {
+        button: {
+          lt_button_text: "Button1",
+          lt_product_link: { route: "Route1" },
+          lt_internal: { referenceId: "Button1" },
+        },
+        description: "Description",
+        picture: {
+          src: "https://placehold.co/600x400",
+          alt: "Alt Text",
+        },
+        title: "Title1",
+      },
+      {
+        button: {
+          lt_button_text: "Button2",
+          lt_product_link: { route: "Route2" },
+          lt_internal: { referenceId: "Button2" },
+        },
+        description: "Description",
+        picture: {
+          src: "http://placehold.co/600x400",
+          alt: "Alt Text",
+        },
+        title: "Title2",
+      },
+    ],
+  },
+};
