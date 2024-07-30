48d619a2a8858c19d9960970d11ea3408dc6c8e0
Alec Winter <alec.winter@monday-consulting.com>
2024-07-30 14:14:16 +0200

    feat(codegen): add codegen to generate types

---
+++ codegen.ts
@@ -0,0 +1,37 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: [
+    {
+      [`${process.env.DATA_LAYER_API_URL}`]: {
+        headers: {
+          Authorization: `Bearer ${process.env.DATA_LAYER_API_TOKEN}`,
+        },
+      },
+    },
+  ],
+  documents: "src/gql/**/*.ts",
+  generates: {
+    "src/gql/generated/types.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-query",
+        {
+          add: {
+            content: "// @ts-nocheck",
+          },
+        },
+      ],
+    },
+  },
+  hooks: {
+    afterOneFileWrite: ["biome format --write ./"],
+  },
+};
+
+export default config;
