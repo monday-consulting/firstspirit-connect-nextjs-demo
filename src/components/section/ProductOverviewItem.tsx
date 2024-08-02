9fe577f84f4271698e027863b30789e64d9de063
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-08-02 15:42:03 +0200

    fix(NLFY-60): (hopefully) last changes to props

--- src/components/section/ProductOverviewItem.tsx
+++ src/components/section/ProductOverviewItem.tsx
@@ -4,7 +4,7 @@ import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { useState } from "react";
 import { Arrow } from "../elements/Arrow";
 
-interface ProductOverviewItemProps {
+export interface ProductData {
   image: {
     src: string;
     alt: string;
@@ -16,14 +16,11 @@ interface ProductOverviewItemProps {
   route?: string;
 }
 
-const ProductOverviewItem = ({
-  image,
-  categories,
-  name,
-  price,
-  id,
-  route,
-}: ProductOverviewItemProps) => {
+interface ProductOverviewItemProps {
+  data: ProductData;
+}
+
+const ProductOverviewItem = ({ data }: ProductOverviewItemProps) => {
   const [active, setActive] = useState<boolean>(false);
 
   function toggleState() {
@@ -34,27 +31,33 @@ const ProductOverviewItem = ({
     <div>
       <div className="mb-24 w-full px-3">
         <div className="mb-9 w-full overflow-hidden rounded-2xl">
-          <Link href={route}>
-            {image && (
-              <Image src={image.src} alt={image.alt} className="w-full" width={400} height={400} />
+          <Link href={data.route}>
+            {data.image && (
+              <Image
+                src={data.image.src}
+                alt={data.image.alt}
+                className="w-full"
+                width={400}
+                height={400}
+              />
             )}
           </Link>
         </div>
       </div>
       <div>
         <div className="text-center">
-          {categories.map((category) => (
+          {data.categories.map((category) => (
             <p key={category} className="m-2 inline font-medium text-gray-400 text-sm">
               {category}
             </p>
           ))}
-          <Link href={route}>
+          <Link href={data.route}>
             <h3 className="my-4 font-heading font-medium text-xl leading-8 hover:underline">
-              {name}
+              {data.name}
             </h3>
           </Link>
           <p className="font-heading font-medium text-gray-900 text-xl tracking-tighter">
-            {price}€
+            {data.price}€
           </p>
         </div>
         <div className="flex flex-wrap items-center text-center text-xl leading-3">
@@ -70,7 +73,7 @@ const ProductOverviewItem = ({
           </div>
           <div className="w-1/2 xl:w-9/12">
             <div className="lg:mx-auto lg:max-w-max xl:mr-0">
-              <Link href={route} className="py-px text-gray-400">
+              <Link href={data.route} className="py-px text-gray-400">
                 <Arrow />
               </Link>
             </div>
