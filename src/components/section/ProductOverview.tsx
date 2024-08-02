61066d0b4de73776e3b5d8318524c41c12e1bef1
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-08-02 16:11:36 +0200

    feat(NLFY-59): added ProductOverview component and story

--- src/components/section/ProductOverview.tsx
+++ src/components/section/ProductOverview.tsx
@@ -1,6 +1,36 @@
-// TODO: crate component
-function ProductOverview() {
-  return null;
+import { ProductOverviewItem, type ProductData } from "./ProductOverviewItem";
+
+interface ProductOverviewProps {
+  products: ProductData[];
 }
 
-export default ProductOverview;
+const ProductOverview = ({ products }: ProductOverviewProps) => {
+  //apply locale to productsFound
+  const productsFound = "Produkte gefunden";
+  return (
+    <section className="bg-white py-24">
+      <div className="container mx-auto px-4 text-center">
+        {products !== null && (
+          <div className="bg-blueGray-100 pt-12 pb-20 2xl:pb-22">
+            <div className="border-black border-b border-opacity-5 pb-9 text-center">
+              <div className="relative">
+                <h2 className="mb-5 text-center font-heading font-medium text-5xl text-gray-900 leading-normal md:mb-0 xl:text-10xl">
+                  {products[0].categories[0]}
+                </h2>
+                <span className="font-medium text-gray-400 text-sm md:absolute md:right-0 md:bottom-3">
+                  {products.length + " " + productsFound}
+                </span>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {products.map((product) => (
+            <ProductOverviewItem key={product.id} data={product} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export { ProductOverview };
---
+++ src/stories/section/ProductOverview.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ProductOverview } from "@/components/section/ProductOverview";
+
+const meta = {
+  title: "components/Section/ProductOverview",
+  component: ProductOverview,
+  tags: ["autodocs"],
+} satisfies Meta<typeof ProductOverview>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    products: [
+      {
+        image: {
+          src: "https://placehold.co/600x400",
+          alt: "Alt Text",
+        },
+        categories: ["Category 1", "Category 2"],
+        name: "Product 1",
+        price: 25,
+        id: "product 1",
+        route: "Product 1",
+      },
+      {
+        image: {
+          src: "https://placehold.co/600x400",
+          alt: "Alt Text",
+        },
+        categories: ["Category 4"],
+        name: "Product 2",
+        price: 50,
+        id: "product 2",
+        route: "Product 2",
+      },
+    ],
+  },
+};
