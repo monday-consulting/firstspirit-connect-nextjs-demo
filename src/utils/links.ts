9c0076aeef6cb01c65f3263b2f70ad23b9602f12
John Rodriguez <john.rodriguez@monday-consulting.com>
2024-09-16 12:27:12 +0200

    fix(NLFY-183): refactor getProductDetailLink function

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,7 +1,9 @@
+import { defaultLocale } from "@/i18n/config";
 import { replaceUmlauts } from "./strings";
 
 export const parseLink = (str: string) => replaceUmlauts(str.toLowerCase().replace(/\s/g, "-"));
 
-export const getProductDetailLink = (id: string) => `/product-detail/${parseLink(id)}`;
+export const getProductDetailLink = (id: string, locale: string) =>
+  `/${locale === defaultLocale ? "produkt-detail" : "product-detail"}/${parseLink(id)}`;
 
 export const getProductGroupLink = (groupName: string) => `${parseLink(groupName)}`;
