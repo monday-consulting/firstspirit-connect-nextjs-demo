dafbae3ada76ac169ca2641e86b60fe6528a1c5f
Alec Winter <alec.winter@monday-consulting.com>
2024-09-18 10:53:23 +0200

    feat(NLFY-159): comment out Section and Home stories for development process

--- src/stories/page-body-count/Section.stories.tsx
+++ src/stories/page-body-count/Section.stories.tsx
@@ -1,42 +1,42 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Section } from "@/components/page-body-count/Section";
+// import type { Meta, StoryObj } from "@storybook/react";
+// import { Section } from "@/components/page-body-count/Section";
 
-const meta: Meta<typeof Section> = {
-  title: "components/PageBodyContent/Section",
-  component: Section,
-  parameters: {
-    layout: "centered",
-  },
-  tags: ["autodocs"],
-};
+// const meta: Meta<typeof Section> = {
+//   title: "components/PageBodyContent/Section",
+//   component: Section,
+//   parameters: {
+//     layout: "centered",
+//   },
+//   tags: ["autodocs"],
+// };
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+// export default meta;
+// type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    section: {
-      id: "exampleId",
-      previewId: "exampleId.de_DE",
-      sectionType: "smartliving.product_overview",
-      type: "Section",
-      data: {
-        entityType: "Header",
-        filterParams: {
-          Primary: true,
-        },
-        maxPageCount: 20,
-        ordering: [
-          {
-            ascending: true,
-            attribute: "asc",
-          },
-        ],
-        query: "",
-        recordCountPerPage: 0,
-        schema: "",
-      },
-      children: [],
-    },
-  },
-};
+// export const Default: Story = {
+//   args: {
+//     section: {
+//       id: "exampleId",
+//       previewId: "exampleId.de_DE",
+//       sectionType: "smartliving.product_overview",
+//       type: "Section",
+//       data: {
+//         entityType: "Header",
+//         filterParams: {
+//           Primary: true,
+//         },
+//         maxPageCount: 20,
+//         ordering: [
+//           {
+//             ascending: true,
+//             attribute: "asc",
+//           },
+//         ],
+//         query: "",
+//         recordCountPerPage: 0,
+//         schema: "",
+//       },
+//       children: [],
+//     },
+//   },
+// };
--- src/stories/page-layout/Home.stories.tsx
+++ src/stories/page-layout/Home.stories.tsx
@@ -1,70 +1,70 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Home } from "@/components/page-layout/Home";
+// import type { Meta, StoryObj } from "@storybook/react";
+// import { Home } from "@/components/page-layout/Home";
 
-const meta = {
-  title: "components/PageLayout/Home",
-  component: Home,
-  tags: ["autodocs"],
-} satisfies Meta<typeof Home>;
+// const meta = {
+//   title: "components/PageLayout/Home",
+//   component: Home,
+//   tags: ["autodocs"],
+// } satisfies Meta<typeof Home>;
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+// export default meta;
+// type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    pageBodies: [
-      {
-        children: [
-          {
-            id: "asdf",
-            type: "Section",
-            name: "stage",
-            displayName: "Stage",
-            sectionType: "stage",
-            previewId: "xyz",
-            data: {
-              st_cta: {
-                type: "Link",
-                template: "internal_link",
-                data: {
-                  lt_link: {
-                    type: "Reference",
-                    referenceId: "09a44011-a593-4b44-96b3-449f21ce6c28",
-                    referenceType: "PageRef",
-                  },
-                  lt_text: "Schauen Sie sich unsere innovativen Lösungen an",
-                },
-              },
-              st_headline: "Geniesse Dein smartes Zuhause",
-              st_image: {
-                type: "Image",
-                id: "qon",
-                previewId: "def",
-                description: "",
-                resolutions: {
-                  ORIGINAL: {
-                    fileSize: 2142271,
-                    extension: "jpg",
-                    mimeType: "image/jpeg",
-                    width: 6000,
-                    height: 4000,
-                    url: "https://images.unsplash.com/photo-1722265733876-45bd4dd9f625?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    seoRoute: "/images/content/innovation.jpg",
-                  },
-                },
-              },
-              st_sectionLifespanFrom: null,
-              st_sectionLifespanTo: null,
-              st_subheadline:
-                "Mit intuitiven Geräten und einfach zu bedienenden Funktionen können sich unsere Kunden ihr individuelles Zuhause gestalten. Von der Lichtsteuerung bis zur zentralen Medienverwaltung.",
-            },
-            displayed: true,
-            children: [],
-          },
-        ],
-        name: "top",
-        previewId: "abc",
-      },
-    ],
-  },
-};
+// export const Default: Story = {
+//   args: {
+//     pageBodies: [
+//       {
+//         children: [
+//           {
+//             id: "asdf",
+//             type: "Section",
+//             name: "stage",
+//             displayName: "Stage",
+//             sectionType: "stage",
+//             previewId: "xyz",
+//             data: {
+//               st_cta: {
+//                 type: "Link",
+//                 template: "internal_link",
+//                 data: {
+//                   lt_link: {
+//                     type: "Reference",
+//                     referenceId: "09a44011-a593-4b44-96b3-449f21ce6c28",
+//                     referenceType: "PageRef",
+//                   },
+//                   lt_text: "Schauen Sie sich unsere innovativen Lösungen an",
+//                 },
+//               },
+//               st_headline: "Geniesse Dein smartes Zuhause",
+//               st_image: {
+//                 type: "Image",
+//                 id: "qon",
+//                 previewId: "def",
+//                 description: "",
+//                 resolutions: {
+//                   ORIGINAL: {
+//                     fileSize: 2142271,
+//                     extension: "jpg",
+//                     mimeType: "image/jpeg",
+//                     width: 6000,
+//                     height: 4000,
+//                     url: "https://images.unsplash.com/photo-1722265733876-45bd4dd9f625?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//                     seoRoute: "/images/content/innovation.jpg",
+//                   },
+//                 },
+//               },
+//               st_sectionLifespanFrom: null,
+//               st_sectionLifespanTo: null,
+//               st_subheadline:
+//                 "Mit intuitiven Geräten und einfach zu bedienenden Funktionen können sich unsere Kunden ihr individuelles Zuhause gestalten. Von der Lichtsteuerung bis zur zentralen Medienverwaltung.",
+//             },
+//             displayed: true,
+//             children: [],
+//           },
+//         ],
+//         name: "top",
+//         previewId: "abc",
+//       },
+//     ],
+//   },
+// };
