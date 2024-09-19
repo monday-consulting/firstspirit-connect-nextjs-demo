303324434f818114b377a12f5bb783fcd05c74e6
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-09-19 20:01:02 +0200

    fix(NLFY-55): added translations for hardcoded ui elements

--- src/components/section/NewsOverview/NewsOverview.tsx
+++ src/components/section/NewsOverview/NewsOverview.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import { CategoryDropdown } from "./CategoryDropdown";
 import { CategoryTabs } from "./CategoryTabs";
 import type { NewsT } from "./NewsTeaser";
@@ -10,14 +11,15 @@ export type NewsOverviewProps = {
 
 const NewsOverview = ({ News }: NewsOverviewProps) => {
   const size = useWindowSize();
+  const t = useTranslations();
 
   return (
     <div>
       <div className="text-center">
-        <h1 className="mb-5 font-bold font-heading text-2xl text-primary">News Overview</h1>
-        <p className="mb-7 text-text">
-          A simple news overview page with search and filter capabilities.
-        </p>
+        <h1 className="mb-5 font-bold font-heading text-2xl text-primary">
+          {t("news.newsOverview")}
+        </h1>
+        <p className="mb-7 text-text">{t("news.subHeadline")}</p>
         <Search className="m-auto mb-10 block sm:w-full md:w-1/3" />
       </div>
       {size.width <= 640 ? <CategoryDropdown News={News} /> : <CategoryTabs News={News} />}
--- src/components/section/NewsOverview/Search.tsx
+++ src/components/section/NewsOverview/Search.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import { BiSearchAlt } from "react-icons/bi";
 
 interface SearchProps {
@@ -5,6 +6,8 @@ interface SearchProps {
 }
 
 const Search = ({ className }: SearchProps) => {
+  const t = useTranslations();
+
   return (
     <div
       className={`flex h-10 items-center rounded-md border-2 border-input pl-3 font-medium text-sm text-text ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2 ${className}`}
@@ -12,7 +15,7 @@ const Search = ({ className }: SearchProps) => {
       <BiSearchAlt className="h-[18px] w-[18px]" />
       <input
         type="search"
-        placeholder="Search"
+        placeholder={t("news.search")}
         className="w-full p-2 placeholder:text-text focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
       />
     </div>
