ba48301f2716ec04ed576ac883beb383b3f19337
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-10-21 13:45:31 +0200

    fix(NLFY-205): inserted ImageComponent in ProductTeaser

--- src/components/elements/ProductTeaser.tsx
+++ src/components/elements/ProductTeaser.tsx
@@ -1,7 +1,7 @@
 import type { ImageData } from "@/types";
-import Image from "next/image";
 import { Link } from "@/i18n/routing";
 import { RichTextElement, type RichTextElementProps } from "./RichTextElement";
+import { ImageComponent } from "./Image";
 
 export type ProductTeaserProps = {
   name: string;
@@ -14,15 +14,12 @@ const ProductTeaser = ({ name, description, route, image }: ProductTeaserProps) 
   return (
     <div className="flex h-fit max-w-[400px] flex-col gap-4 rounded-xl bg-white p-6 shadow-lg">
       <Link href={route}>
-        <div className="w-full overflow-hidden rounded-xl">
-          <Image
-            src={image.src}
-            alt={image.alt}
-            className="h-auto max-h-64 w-full object-cover"
-            width={400}
-            height={400}
-          />
-        </div>
+        <ImageComponent
+          src={image.src}
+          alt={image.alt}
+          rounded="xl"
+          className="w-full [aspect-ratio:1/1]"
+        />
         <h1 className="mt-4 font-bold text-2xl">{name}</h1>
       </Link>
       <RichTextElement {...description} className="line-clamp-5 text-text" />
