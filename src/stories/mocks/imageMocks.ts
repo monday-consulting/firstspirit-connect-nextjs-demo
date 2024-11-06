0f65ed601f7a1c66d29b125954e227b5df599424
Letizia Schachten <letizia.schachten@monday-consulting.com>
2024-11-06 12:22:39 +0100

    feat(NLFY-228): added mock files for images, texts and titles

---
+++ src/stories/mocks/imageMocks.ts
@@ -0,0 +1,29 @@
+export const TinyImage = {
+  src: "http://placehold.co/50x50",
+  alt: "This is a tiny image",
+};
+
+export const SmallImage = {
+  src: "http://placehold.co/100x100",
+  alt: "This is a small image",
+};
+
+export const DefaultImage = {
+  src: "http://placehold.co/600x400",
+  alt: "This is the default image",
+};
+
+export const WideImage = {
+  src: "http://placehold.co/1200x400",
+  alt: "This is a wide image",
+};
+
+export const HighImage = {
+  src: "http://placehold.co/400x1200",
+  alt: "This is a high image",
+};
+
+export const HugeImage = {
+  src: "http://placehold.co/4000x4000",
+  alt: "This is a huge image",
+};
