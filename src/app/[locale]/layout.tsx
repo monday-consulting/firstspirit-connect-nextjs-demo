2df0dedf80f03a8cba15b6a1d61bd5c0b41374bb
Alec Winter <alec.winter@monday-consulting.com>
2025-04-23 11:32:50 +0200

    feat(TPP): add query for bodyPreviewId

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -13,6 +13,7 @@ import { Footer } from "@/components/layouts/Footer";
 import type { LinkData } from "@/types";
 import Script from "next/script";
 import { getPreviewParams } from "@/utils/preview/getPreviewParams";
+import { getBodyPreviewId } from "@/gql/documents/bodyPreviewId";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,12 +29,12 @@ const RootLayout = async (
   props: Readonly<{
     children: React.ReactNode;
     params: Promise<{ locale: Locale }>;
-  }>,
-  previewId?: string
+  }>
 ) => {
-  const previewProps = getPreviewParams(previewId);
   const { children } = props;
   const { locale } = await props.params;
+
+  const previewProps = getPreviewParams(await getBodyPreviewId(locale));
 
   const messages = await getMessages();
   const structure = await getNavigationStructure(locale);
