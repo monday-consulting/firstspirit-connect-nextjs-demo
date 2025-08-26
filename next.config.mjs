c5f96639ca58ee7acaea7d82f7de3aca917f2f40
John Rodriguez <john.rodriguez@monday-consulting.com>
2025-08-26 10:28:22 +0200

    chore(mcp): updated nextjs configs image path

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,23 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin({
-  experimental: {
-    createMessagesDeclaration: "./messages/en-GB.json",
-  },
+    experimental: {
+        createMessagesDeclaration: "./messages/en-GB.json",
+    },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "monday-dev-caas-api.e-spirit.cloud",
-      },
-      {
-        protocol: "https",
-        hostname: "partnerprod-media.e-spirit.cloud",
-      },
-      { protocol: "https", hostname: "maps.googleapis.com" },
-    ],
-  },
+    trailingSlash: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "mondaydev-media.e-spirit.cloud",
+            },
+            { protocol: "https", hostname: "maps.googleapis.com" },
+        ],
+    },
 };
 
 export default withNextIntl(nextConfig);
